02f4249937d19b2f87cb5a2fcacb2974d3804330
Genesis <genesisybenitez1@gmail.com>
2022-04-21 23:58:38 -0400

    updated everything

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -11,6 +11,7 @@ function Book({userId}){
 
     const [book, setBook] = useState([]);
     const [bookComments, setBookComments] = useState([]);
+    const [bookRatings, setBookRatings] = useState([]);
     const [errorMsg, setErrorMsg] = useState("");
 
     //add comment fields
@@ -36,10 +37,41 @@ function Book({userId}){
             console.log(error);
         })
     }
+    const getBookRatings = () =>{
+        axios.get(`http://localhost:8080/bookComments/getBookRatingInfoByBookId/${id}`, {withCredentials: true})
+        .then(function(response){
+            console.log(response.data);
+            setBookRatings(response.data[0]);
+        }).catch(function(error){
+            console.log(error);
+        })
+    }
     const textareaStyle = {height: "100px"}
 
     useEffect(()=> getBook(), []);
     useEffect(()=> getBookComments(), []);
+    useEffect(()=> getBookRatings(), []);
+
+
+    function getAvgRatingForProgressBar(total_ratings, starRatings){
+            let percentage = (100 * starRatings) / total_ratings;
+            return {width: `${percentage}0%`}
+    
+    }
+
+    function isPlural(num){
+        if(num == 1){
+            return "";
+        }else{
+            return "s";
+        }
+    }
+
+    function convertTime(time){
+        var newTime = new Date(time);
+        const realTime = newTime.toLocaleString('en-US', {month: 'long', day: 'numeric', year:'numeric', hour: 'numeric', minute: 'numeric'})
+        return realTime;
+    }
 
     //handle submit
     const addBookCommentSubmit = (e) =>{
@@ -60,6 +92,8 @@ function Book({userId}){
             comment: comment
         }).then(function(response){
             console.log(response);
+            setBookRatings([]);
+            getBookRatings();
             setBookComments([]);
             getBookComments();
             document.getElementById('closeBookCommentModal').click();
@@ -99,29 +133,100 @@ function Book({userId}){
                         <small className='d-block'>{book.description}</small>
                         <small className='d-block'><GrLanguage/> {book.language}</small>
                     </div>
-                    <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 mt-4'>
+                    <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 mt-4 mb-4'>
                         <img src={book.cover} className='w-100 mw-100'/>
                     </div>
                 </div>
             </div>
-            <div className='col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 px-5 pt-5'>
-                {bookComments.map((bookComment, i)=>(
-                    <div className='row'>
-                        <div className='col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10'>   
-                            <img src={bookComment.avatar} height="35" width="35" className='rounded-circle'/>
-                            <small className='lead fw-bold px-4'>{bookComment.username} </small>
+            <div className='col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 mb-4'>
+                <div className='row justify-content-center mt-4'>
+                    <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4'>
+                        <small className='lead'>Reviews</small>
+                        <div className='row justify-content-center'>
+                            <div className='col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 '>
+                            <ReactStars
+                                            count={bookRatings.avg_rating}
+                                            size={16}
+                                            color="#ffd700"
+                                            
+                                            classNames="mt-2"
+                                    
+                                        />
+                            </div>
+                            <div className='col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+                                <small className='d-block mt-2'>{bookRatings.avg_rating} out of 5</small>
+                            </div>
                         </div>
-                        <div className='col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 px-5'>   
-                        <ReactStars
+                        <small className='d-block mt-2'>{bookRatings.total_ratings} rating{isPlural(bookRatings.total_ratings)}</small>
+                        <div className='row justify-content-center mt-2'>
+                            <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 '>
+                                <small>5 star</small>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" style={getAvgRatingForProgressBar(bookRatings.total_ratings, bookRatings.five_star_ratings)} role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 '>
+                                <small>4 star</small>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning " style={getAvgRatingForProgressBar(bookRatings.total_ratings, bookRatings.four_star_ratings)}role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 '>
+                                <small>3 star</small>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning " style={getAvgRatingForProgressBar(bookRatings.total_ratings, bookRatings.three_star_ratings)}role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 '>
+                                <small>2 star</small>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning " style={getAvgRatingForProgressBar(bookRatings.total_ratings, bookRatings.two_star_ratings)}role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 '>
+                                <small>1 star</small>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning "style={getAvgRatingForProgressBar(bookRatings.total_ratings, bookRatings.one_star_ratings)} role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 '>
+                                <small className='lead'>Comments</small>
+                                {bookComments.map((bookComment, i)=>(  
+                                <div className='row justify-content-center mt-4'>
+                                    <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 ' align="center">
+                                        <img src={bookComment.avatar} height="35" width="35" className='rounded-circle'/>
+                                    </div>
+                                    <div className='col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8'>
+                                        <small className='fw-bold '>{bookComment.username} </small>
+                                        <small className='px-2'>{convertTime(bookComment.created_at)}</small>
+                                        <ReactStars
                                         count={bookComment.rating}
                                         size={18}
                                         color="#ffd700"
                                     />
-                        <small>{bookComment.comment}</small>
-                        </div>
-                     </div>
-                ))}
+                                        <small className='d-block'>{bookComment.comment}</small>
+                                    </div>
+                                </div>  
+                                ))}        
+                    </div>
+                     
+                                    
+                    
+                </div>
             </div>
+
         </div>
         <div className='my modals'>
 
